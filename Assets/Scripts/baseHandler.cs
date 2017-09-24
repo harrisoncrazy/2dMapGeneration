6641c41ec182ed8a47d9b8639af3a35f43d54f54
harrisoncrazy <harrisoncrawford16@yahoo.ca>
2017-09-24 14:37:40 -0400

    default parent/child

--- Assets/Scripts/baseHandler.cs
+++ Assets/Scripts/baseHandler.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class baseHandler : MonoBehaviour {
-
-	public tileHandler.gridPosition basePosition = new tileHandler.gridPosition();
-
-	public GameObject tileOutlineSprite;
-
-	//Tile selection values
-	private Transform trSelect = null;
-	private bool selected = false;
+public class baseHandler : defaultBuilding {
 
 	// Use this for initialization
 	void Start () {
@@ -18,26 +10,12 @@ public class baseHandler : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (GameManager.Instance.selectedTile != null) {
-			trSelect = GameManager.Instance.selectedTile;
-		}
-		//Swapping selected tile
-		if (selected == true && this.transform != trSelect) { //If the currently selected tile and the transform do not equal the new selection, deselect this tile
-			selected = false;
-			tileOutlineSprite.SetActive (false);
-		}
+	protected override void Update() {
+		base.Update ();
 	}
 
-	public void OnMouseDown() {
-		if (selected && transform == trSelect) {
-			selected = false;
-			trSelect = null;
-			tileOutlineSprite.SetActive (false);
-		} else {
-			selected = true;
-			GameManager.Instance.selectedTile = this.transform;
-			tileOutlineSprite.SetActive (true);
-		}
+	protected override void OnMouseDown() {
+		base.OnMouseDown ();
+
 	}
 }
---
+++ Assets/Scripts/defaultBuilding.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defaultBuilding : MonoBehaviour {
+
+	public tileHandler.gridPosition basePosition = new tileHandler.gridPosition();
+
+	public GameObject tileOutlineSprite;
+
+	//Tile selection values
+	private Transform trSelect = null;
+	private bool selected = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		if (GameManager.Instance.selectedTile != null) {
+			trSelect = GameManager.Instance.selectedTile;
+		}
+		//Swapping selected tile
+		if (selected == true && this.transform != trSelect) { //If the currently selected tile and the transform do not equal the new selection, deselect this tile
+			selected = false;
+			tileOutlineSprite.SetActive (false);
+		}
+	}
+
+	protected virtual void OnMouseDown() {
+		if (selected && transform == trSelect) {
+			selected = false;
+			trSelect = null;
+			tileOutlineSprite.SetActive (false);
+		} else {
+			selected = true;
+			GameManager.Instance.selectedTile = this.transform;
+			tileOutlineSprite.SetActive (true);
+		}
+	}
+}
